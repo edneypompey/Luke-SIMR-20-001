c698e86318888d9c4eecd4c2ce24dadc016b272f
luke edney <edneypompey@hotmail.co.uk>
2021-09-27 13:35:31 +0100

    4.3.1.6 LAB

--- Module 4/Module_4.py
+++ Module 4/Module_4.py
@@ -2,5 +2,19 @@
 # Module 4
 # Luke Edney
 
+# 4.3.1.6 LAB: A leap year: writing your own functions
 
+# (Simplified variant of the task to make it understandable for myself)
+# write and test a function which takes one argument (a year)
+# and returns True if the year is an even year, or False otherwise.
 
+def is_year_even(year): # define the function, with a year (parameter)
+    if year % 2 == 0:   # if the year is even (conditions are met)
+        return True     # terminate the function's execution, with True
+    else:               # otherwise
+        return False    # terminate the function's execution, with False
+        
+print(is_year_even(1900))   # printing test data
+print(is_year_even(2000))
+print(is_year_even(2016))
+print(is_year_even(1987))
